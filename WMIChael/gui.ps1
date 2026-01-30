d7d6b7c1235816709621ac7a67714f76adf0ba9a
Varufaker <62913295+Varufaker@users.noreply.github.com>
2026-01-30 09:27:35 +0100

    Add files via upload

--- WMIChael/gui.ps1
+++ WMIChael/gui.ps1
@@ -24,51 +24,107 @@ Add-Type -AssemblyName System.Windows.Forms
 $gui = New-Object System.Windows.Forms.Form
 $gui.Text = “WMIChael a0.1”
 $gui.Width = 720
-$gui.Height = 380
+$gui.Height = 440
 $gui.StartPosition = “CenterScreen”
 
 #Tab Control
 $tabs = New-Object System.Windows.Forms.TabControl 
-$tabs.Size = New-Object System.Drawing.Size(580,360) 
+$tabs.Size = New-Object System.Drawing.Size(680,360) 
 $tabs.Location = New-Object System.Drawing.Point(10,10)
-# Crear pestaña 1 
-$tab1 = New-Object System.Windows.Forms.TabPage 
-$tab1.Text = "General" 
-# Añadir un control a la pestaña 1 
-$label1 = New-Object System.Windows.Forms.Label 
-$label1.Text = "Contenido de la pestaña General" 
-$label1.Location = New-Object System.Drawing.Point(20,20) 
-$tab1.Controls.Add($label1) 
-# Crear pestaña 2 
-$tab2 = New-Object System.Windows.Forms.TabPage 
-$tab2.Text = "Opciones" 
-# Añadir un control a la pestaña 2 
-$button1 = New-Object System.Windows.Forms.Button 
-$button1.Text = "Pulsar" 
-$button1.Location = New-Object System.Drawing.Point(20,20) 
-$tab2.Controls.Add($button1) 
-# Añadir pestañas al TabControl 
-$tabs.TabPages.Add($tab1) 
-$tabs.TabPages.Add($tab2)
 
-    #Window Content
-    $label = New-Object System.Windows.Forms.Label
-    $label.Text = "EQUIPO`nNombre del equipo: $(Get-Pcname)`nGrupo de Trabajo: $(Get-Workgroup) | Dominio: $(Get-Domain)`n`nAntivirus: $(Get-AVSoft)`n`n"
-    $label.AutoSize = $true
-    $label.Location = New-Object System.Drawing.Point(10,20)
-
-    #CPU Block
+    # Create Tab PC
+    $tabPC = New-Object System.Windows.Forms.TabPage 
+    $tabPC.Text = "PC"
+    # PC Tab controls
+    #PCBlock
+    $labelPC = New-Object System.Windows.Forms.Label 
+    $labelPC.Text = "EQUIPO`nNombre del equipo: $(Get-Pcname)`nGrupo de Trabajo: $(Get-Workgroup) | Dominio: $(Get-Domain)`n`nAntivirus: $(Get-AVSoft)"
+    $labelPC.AutoSize = $true
+    $labelPC.Location = New-Object System.Drawing.Point(20,20)
+    #CPUBlock
     $cpu = Get-Cpuinfo
-    $cpu_block = New-Object System.Windows.Forms.Label
-    $cpu_block.Text = "CPU`nNombre: $($cpu.Nombre)`nNucleos: $($cpu.Nucleos)`nHilos: $($cpu.Hilos)`nVelocidad: $($cpu.Velocidad)`nFabricante: $($cpu.Fabricante)"
-    $cpu_block.AutoSize = $true
-    $cpu_block.Location = New-Object System.Drawing.Point(10,120)
-	
-	#Memory Block
-	$mem_block = New-Object System.Windows.Forms.label
-	$mem_block.Text = "Memory block"
-	$mem_block.AutoSize = $true
-	$mem_block.Location = New-Object System.Drawing.Point(10,230)
+    $labelCPU = New-Object System.Windows.Forms.Label
+    $labelCPU.Text = "CPU`nNombre: $($cpu.Nombre)`nNucleos: $($cpu.Nucleos)`nHilos: $($cpu.Hilos)`nVelocidad: $($cpu.Velocidad)`nFabricante: $($cpu.Fabricante)"
+    $labelCPU.Autosize = $true
+    $labelCPU.Location = New-Object System.Drawing.Point(20,120)
+    #Adding controls to tab
+    $tabPC.Controls.Add($labelPC)
+    $tabPC.Controls.Add($labelCPU)
+
+    # Create Tab RAM 
+    $tabRAM = New-Object System.Windows.Forms.TabPage 
+    $tabRAM.Text = "Memorias" 
+    # RAM Tab controls 
+    $labelRAM = New-Object System.Windows.Forms.Label  
+    $labelRAM.Text = "Contenido de la pestaña Memorias"
+    $labelRAM.Autosize = $true
+    $labelRAM.Location = New-Object System.Drawing.Point(20,20)
+    #Adding controls to tab
+    $tabRAM.Controls.Add($labelRAM)
+
+    # Create Tab Disks
+    $tabDisks = New-Object System.Windows.Forms.TabPage
+    $tabDisks.Text = "Discos"
+    # Disks Tab controls
+    $labelDisks = New-Object System.Windows.Forms.Label
+    $labelDisks.Text = "Contenido de la pestaña Discos"
+    $labelDisks.Autosize = $true
+    $labelDisks.Location = New-Object System.Drawing.Point(20,20)
+    #Adding controls to tab
+    $tabDisks.Controls.Add($labelDisks)
+
+    # Create Tab Sound
+    $tabSound = New-Object System.Windows.Forms.TabPage
+    $tabSound.Text = "Sonido"
+    # Sound Tab controls
+    $labelSound = New-Object System.Windows.Forms.Label
+    $labelSound.Text = "Contenido de la pestaña Sonido"
+    $labelSound.Autosize = $true
+    $labelSound.Location = New-Object System.Drawing.Point(20,20)
+    #Adding controls to tab
+    $tabSound.Controls.Add($labelSound)
+
+    # Create Tab Net
+    $tabNet = New-Object System.Windows.Forms.TabPage
+    $tabNet.Text = "Red"
+    # Net Tab controls
+    $labelNet = New-Object System.Windows.Forms.Label
+    $labelNet.Text = "Contenido de la pestaña Red"
+    $labelNet.Autosize = $true
+    $labelNet.Location = New-Object System.Drawing.Point(20,20)
+    #Adding controls to tab
+    $tabNet.Controls.Add($labelNet)
+
+    # Create Tab Others
+    $tabOthers = New-Object System.Windows.Forms.TabPage
+    $tabOthers.Text = "Otros"
+    # Others Tab controls
+    $labelOthers = New-Object System.Windows.Forms.Label
+    $labelOthers.Text = "Contenido de la pestaña Otros"
+    $labelOthers.Autosize = $true
+    $labelOthers.Location = New-Object System.Drawing.Point(20,20)
+    #Adding controls to tab
+    $tabOthers.Controls.Add($labelOthers)
+
+    # Create Tab Options
+    $tabOptions = New-Object System.Windows.Forms.TabPage
+    $tabOptions.Text = "Opciones"
+    # Options Tab controls
+    $labelOptions = New-Object System.Windows.Forms.Label
+    $labelOptions.Text = "Contenido de la pestaña Opciones"
+    $labelOptions.Autosize = $true
+    $labelOptions.Location = New-Object System.Drawing.Point(20,20)
+    #Adding controls to tab
+    $tabOptions.Controls.Add($labelOptions)
+
+    # Adding tabs to TabControl
+    $tabs.TabPages.Add($tabPC)
+    $tabs.TabPages.Add($tabRAM)
+    $tabs.TabPages.Add($tabDisks)
+    $tabs.TabPages.Add($tabSound)
+    $tabs.TabPages.Add($tabNet)
+    $tabs.TabPages.Add($tabOthers)
+    $tabs.TabPages.Add($tabOptions)
 
     #Exit button
     $exitButton = New-Object System.Windows.Forms.Button
@@ -79,7 +135,6 @@ $tabs.TabPages.Add($tab2)
     $exitButton.Add_Click({$gui.Close()})
 
 $gui.Controls.Add($tabs)
-$gui.Controls.Add($label)
 $gui.Controls.Add($cpu_block)
 $gui.Controls.Add($mem_block)
 $gui.Controls.Add($exitButton)
